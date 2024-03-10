440244e2933fcb80d1d780d0b8add0d6e780d47b
Fattah Salleh <fattah.sallehuddin@gmail.com>
2024-03-10 20:11:04 +0800

    Portfolio version 2.0 2024 init

---
+++ my-portfolio/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fattah Salleh Portfolio",
+  description: "Fattah Salleh's portfolio created in 2024 using Next JS",
+  icons: [
+    {
+      rel: 'icon',
+      url: '/images/fs-icon.svg',
+      sizes: 'any',
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+      {/* <body className="Poppins">{children}</body> */}
+    </html>
+  );
+}
