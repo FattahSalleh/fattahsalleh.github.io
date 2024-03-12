a96706e4740a85fa0f80cc441c8d2cc8d17a22e6
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-03-12 15:56:28 +0800

    setup for github pages

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	basePath: "/my-portfolio",
+	output: "export",
+	reactStrictMode: true,
+};
 
 export default nextConfig;
