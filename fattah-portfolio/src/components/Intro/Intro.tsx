42577ee70e4fed471c4ef1d3b0bb9a840cda2920
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-06 01:49:53 +0800

    feat: create new Intro component

---
+++ fattah-portfolio/src/components/Intro/Intro.tsx
@@ -0,0 +1,18 @@
+function Intro() {
+	return (
+		<div
+			className="w-full min-h-screen flex flex-col items-center justify-center pb-24"
+			id="firstSectionContent"
+		>
+			<div
+				className="font-bold h-full text-9xl tracking-wider leading-none drop-shadow-md"
+				id="firstSectionContent-title"
+			>
+				FATTAH SALLEH
+			</div>
+			<div className="font-bold pt-16">FULL STACK DEVELOPER</div>
+		</div>
+	);
+}
+
+export default Intro;
