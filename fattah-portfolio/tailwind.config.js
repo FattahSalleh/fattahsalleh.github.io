58c180c89d119f1ce5f582e2fe9f0881a434e4d4
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-06 03:23:11 +0800

    style: add custom colors to tailwind

--- fattah-portfolio/tailwind.config.js
+++ fattah-portfolio/tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+		colors: {
+			primary: "#2E4B35",
+			secondary: "#50C878",
+			lightBg: "",
+			lightText: "#FEFEFE",
+			darkBg: "#191A1D",
+			darkText: "",
+			greenLine: "#83CD29",
+			blueLine: "#149ECA",
+		},
 		extend: {},
 	},
 	plugins: [],
