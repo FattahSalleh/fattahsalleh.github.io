36215674091782a4a3651083e834db69bf348600
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-05 16:09:53 +0800

    fix: made sure tailwind css works

--- fattah-portfolio/src/App.tsx
+++ fattah-portfolio/src/App.tsx
@@ -4,22 +4,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p className="text-3xl font-bold underline text-red-600">
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<h1 className="text-3xl font-bold underline text-red-600">
+					Simple React Typescript Tailwind Sample
+				</h1>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+			<h1 className="text-3xl font-bold underline text-blue-400">Hello world!</h1>
+		</div>
   );
 }
 
