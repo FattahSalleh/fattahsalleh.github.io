62a1b24e2f96dd4c95a6775d7af4d70e5f9645e9
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-06 03:24:11 +0800

    style: improve tailwind in App

--- fattah-portfolio/src/components/App/App.tsx
+++ fattah-portfolio/src/components/App/App.tsx
@@ -1,22 +1,16 @@
 import Intro from "../Intro/Intro";
 import TopNavBar from "../TopNavBar/TopNavBar";
-import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
 			<main
-				className="flex min-h-screen flex-col items-center justify-between bg-gray-300"
+				className="flex min-h-screen flex-col items-center justify-between bg-darkBg text-lightText"
 				id="main"
 			>
-				<div
-					className="z-10 max-w-full w-full text-base bg-green-950"
-					id="firstSection"
-				>
 					<TopNavBar />
 					<Intro />
-				</div>
-				<div
+				<section
 					className="z-10 max-w-full w-full text-base bg-lime-300 p-12"
 					id="secondSection"
 				>
@@ -25,7 +19,7 @@ function App() {
 					tempor duis ipsum ad fugiat officia eu dolore ipsum. Magna
 					dolor magna enim et occaecat nostrud enim nostrud. Pariatur
 					eiusmod velit culpa elit labore ullamco.
-				</div>
+				</section>
 			</main>
 		</div>
 	);
