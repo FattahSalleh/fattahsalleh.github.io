3b6872d7bc0075f03dab14eeb464a8ea6e516cf5
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-06 00:30:14 +0800

    chore: remove template in App.tsx

--- fattah-portfolio/src/App.tsx
+++ fattah-portfolio/src/App.tsx
@@ -1,28 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p className="text-3xl font-bold underline text-red-600">
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<h1 className="text-3xl font-bold underline text-red-600">
-					Simple React Typescript Tailwind Sample
-				</h1>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-			<h1 className="text-3xl font-bold underline text-blue-400">Hello world!</h1>
+    <div className="App">
+      Test
 		</div>
   );
 }
