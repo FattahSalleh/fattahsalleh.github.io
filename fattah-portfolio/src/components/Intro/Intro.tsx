7dc5774c98c5d0a7b7169787b9a53567b9d55bbc
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-06 15:47:19 +0800

    refactor: add subTitle property for Intro comp

--- fattah-portfolio/src/components/Intro/Intro.tsx
+++ fattah-portfolio/src/components/Intro/Intro.tsx
@@ -1,13 +1,13 @@
-function Intro() {
+function Intro({ name, subTitle }: { name: string; subTitle: string }) {
 	return (
 		<>
 			<div
-				className="font-bold h-full text-9xl tracking-wider leading-none drop-shadow-md"
+				className="font-bold h-full text-9xl tracking-wider leading-none drop-shadow-md uppercase"
 				id="firstSectionContent-title"
 			>
-				FATTAH SALLEH
+				{name}
 			</div>
-			<div className="font-semibold pt-8 text-lg">FULL STACK DEVELOPER &nbsp; & &nbsp; SOFTWARE ENGINEER</div>
+			<div className="font-semibold pt-8 text-lg">{subTitle}</div>
 		</>
 	);
 }
