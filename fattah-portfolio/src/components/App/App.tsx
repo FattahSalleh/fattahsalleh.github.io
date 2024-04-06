855a3214803e18db1f675024707c99cfca178287
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-06 18:04:44 +0800

    feat: add data to Experience table comp

--- fattah-portfolio/src/components/App/App.tsx
+++ fattah-portfolio/src/components/App/App.tsx
@@ -1,7 +1,9 @@
-import { ButtonProps, MyDetails } from "../../common/types";
+import { ButtonProps, Expertise, MyDetails } from "../../common/types";
 import ExpertiseTable from "../ExpertiseTable/ExpertiseTable";
 import Intro from "../Intro/Intro";
 import TopNavBar from "../TopNavBar/TopNavBar";
+import frontendIcon from "../../assets/images/expertise/frontend-icon.svg";
+import backendIcon from "../../assets/images/expertise/backend-icon.svg";
 
 function App() {
 	const buttons: ButtonProps[] = [
@@ -17,6 +19,27 @@ function App() {
 		position: "FULL STACK DEVELOPER \xa0&\xa0 SOFTWARE ENGINEER",
 	};
 
+	const expertiseFE: Expertise = {
+		src: frontendIcon,
+		alt: "Frontend Icon",
+		title: "Frontend",
+		desc: "I have always loved seeing the things I created come to life. With an eye for design, I have always strived to craft the optimal experience for my clients that perfectly aligns with their needs.",
+		subItems: [
+			{
+				title: "Languages",
+				list: ["HTML & CSS", "JavaScript", "TypeScript"],
+			},
+			{
+				title: "Frameworks",
+				list: ["React", "Vue"],
+			},
+			{
+				title: "Tools",
+				list: ["OpenText CMS", "VSCode", "Figma / Framer"],
+			},
+		],
+	};
+
 	return (
 		<div className="App">
 			<main
@@ -28,7 +51,10 @@ function App() {
 					className="w-full min-h-screen flex flex-col items-center justify-center pb-24"
 					id="firstSection"
 				>
-					<Intro name={introDetails.name} subTitle={introDetails.position} />
+					<Intro
+						name={introDetails.name}
+						subTitle={introDetails.position}
+					/>
 				</section>
 
 				<section
@@ -39,7 +65,7 @@ function App() {
 						<p className="font-bold h-full text-7xl drop-shadow-md mb-16">
 							My Expertise
 						</p>
-						<ExpertiseTable />
+						<ExpertiseTable expertise={expertiseFE} />
 					</div>
 				</section>
 			</main>
