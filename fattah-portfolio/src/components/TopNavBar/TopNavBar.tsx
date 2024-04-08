d2f944957e6a613442329c4d47e449ffa84fc156
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-08 15:19:17 +0800

    style: change topnavbar opacity on scroll

--- fattah-portfolio/src/components/TopNavBar/TopNavBar.tsx
+++ fattah-portfolio/src/components/TopNavBar/TopNavBar.tsx
@@ -30,7 +30,7 @@ function TopNavBar({ buttons }: { buttons: ButtonProps[] }) {
 			className={`max-h-24 w-full flex flex-no-wrap fixed top-0 z-10 flex-row justify-between px-12 py-4 items-center transition-all duration-1000 
             ${
 				isScrolled
-					? "bg-darkBg text-secondary"
+					? "bg-darkBg text-secondary opacity-95"
 					: "bg-darkBg text-lightText"
 			}`}
 			id="topNavBar"
