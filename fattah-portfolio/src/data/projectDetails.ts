2e5cc2fd259aabe14387274b03330777aee454da
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-08 22:45:46 +0800

    feat: add projectDetails data

---
+++ fattah-portfolio/src/data/projectDetails.ts
@@ -0,0 +1,11 @@
+import { Projects } from "../common/types";
+import allianceBizsmart from "../assets/images/project/alliance_bizsmart.png";
+
+export const projectAllianceBS: Projects = {
+	src: allianceBizsmart,
+	alt: "Alliance Bank BizSmart",
+	title: "Alliance Bank BizSmart",
+	desc: "The Alliance BizSmart® is your daily business banking solution at your fingertips.",
+	techStack: ["jQuery", "JavaScript", "Test", "Test", "Test", "Test", "Test"],
+	url: "https://bizsmartsolution.alliancebank.com.my/banking/",
+};
