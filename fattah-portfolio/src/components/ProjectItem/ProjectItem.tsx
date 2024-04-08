c8862eb2fdafebf36c8558b9736fc26e82eb40b9
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-08 22:46:27 +0800

    style: adjust styling for ProjectItem component

--- fattah-portfolio/src/components/ProjectItem/ProjectItem.tsx
+++ fattah-portfolio/src/components/ProjectItem/ProjectItem.tsx
@@ -1,26 +1,40 @@
-import allianceBizsmart from "../../assets/images/project/alliance_bizsmart.png";
+import { useState } from "react";
+import { Projects } from "../../common/types";
 
-function ProjectItem() {
+function ProjectItem({ project }: { project: Projects }) {
+	const [isHovered, setIsHovered] = useState(false);
+
 	return (
-		<div className="flex flex-col border-2 rounded-lg w-full items-center bg-primary">
+		<div
+			className="flex flex-col border-2 rounded-lg w-full bg-primary text-start overflow-hidden"
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
 			<div className="min-w-full w-full h-3/4 bg-lightBg">
-				<img src={allianceBizsmart} alt="Alliance Bizsmart" />
+				<img
+					src={project.src}
+					alt={project.alt}
+					className={`transform transition-transform duration-500 ${
+						isHovered ? "scale-110" : "scale-100"
+					}`}
+				/>
 			</div>
-			<div className="h-full min-w-full px-12 py-4 ">
-				<p className="font-bold text-2xl">title</p>
-				<small className="mt-4">desc</small>
-			</div>
-
-			{/* {expertise.subItems.map((subItem, index) => (
-				<div key={index} className="mt-8 text-center">
-					<p className="font-bold">{subItem.title}:</p>
-					<ul>
-						{subItem.list.map((item, idx) => (
-							<li key={idx}>{item}</li>
+			<div className="h-full px-12 py-4 ">
+				<p className="font-bold text-lg">{project.title}</p>
+				{/* <small className="mt-4">{project.desc}</small> */}
+				<div className="font-medium my-1">
+					<ul className="flex flex-wrap flex-row">
+						{project.techStack.map((tech, index) => (
+							<li
+								key={index}
+								className="text-sm bg-blueLine opacity-85 mr-2 my-1 px-4 py-0.5 border rounded-2xl"
+							>
+								{tech}
+							</li>
 						))}
 					</ul>
 				</div>
-			))} */}
+			</div>
 		</div>
 	);
 }
