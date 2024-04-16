3cd9ad6e51d42a451db79c5ac5704280e494ddbe
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-16 10:54:05 +0800

    feat: add init experienceDetails data

---
+++ fattah-portfolio/src/data/experienceDetails.ts
@@ -0,0 +1,49 @@
+import { Experience } from "../common/types";
+
+export const experience: Experience[] = [
+	{
+		position: "Web Developer (UI/UX Designer / Frontend Developer)",
+		company: "Katsana Fleet Services",
+		timeRange: "November 2023 - Current",
+		responsibility: [
+			"Designed cohesive websites, tablet, and mobile mock-ups using Figma, while also presenting design concepts and proposals to management for approval and implementation.",
+			"Led UI/UX enhancements for both existing and new products, guiding the process from conceptualization to high-fidelity web designs and prototypes.",
+			"Developed reusable components using Vue.js while delving into PHP Laravel, elevating user experience and interactions through refined coding practices.",
+		],
+		techStack: ["Tech1", "Tech2"],
+		url: "",
+	},
+	{
+		position: "Digital Developer",
+		company: "Trinergy Digital",
+		timeRange: "March 2021 - October 2023",
+		responsibility: [
+			"Contributed to over 6 projects using various libraries and frameworks such as jQuery, Pug, Vue, React, NodeJS and Bootstrap, ensuring the creation of responsive, error-free and intuitive websites experiences tailored to the specific needs of banking clients.",
+			"Pioneered the implementation of OpenText CMS, leading a team of developers using Agile methodology to revamp corporate websites while maintaining existing functionality.",
+			"Trained customers and team members on website maintenance and updates while creating comprehensive documentation, increasing maintainability and reduction in support requests.",
+			"Built an Online Banking DuitNow QR native mobile application using Kotlin in Android Studio",
+		],
+		techStack: ["Tech1", "Tech2"],
+		url: "",
+	},
+	{
+		position: "Technical Support Executive - New Zealand Department",
+		company: "Fuji Xerox / FujiFilm",
+		timeRange: "July 2020 - February 2021",
+		responsibility: [
+			"Provided technical support to customers from New Zealand, troubleshooting hardware and software issues, resulting in a 95% customer satisfaction rating.",
+		],
+		techStack: ["Tech1", "Tech2"],
+		url: "",
+	},
+	{
+		position: "Internship",
+		company: "Telekom Malaysia Alpha Lab",
+		timeRange: "April 2019 - June 2019",
+		responsibility: [
+			"Researched and implemented front-end enhancements for the company’s web portal via ColdFusion, making the portal more user friendly and accessible.",
+		],
+		techStack: ["Tech1", "Tech2"],
+		url: "",
+	},
+];
