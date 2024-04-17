94d1bb078c51126b149f0c4abb318609250295ac
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-17 09:59:24 +0800

    style: add animation to open accordion

--- fattah-portfolio/src/components/ExperienceList/ExperienceList.tsx
+++ fattah-portfolio/src/components/ExperienceList/ExperienceList.tsx
@@ -30,50 +30,68 @@ function ExperienceList({
 				</div>
 				<div className="flex justify-between items-center">
 					<div className="mr-8">{experience.timeRange}</div>
-					<div className="text-base h-auto w-4">{isOpen ? <img src={minusIcon} alt="Minus Icon" /> : <img src={plusIcon} alt="Plus Icon" /> }</div>
+					<div className="text-base h-auto w-4">
+						{isOpen ? (
+							<img src={minusIcon} alt="Minus Icon" />
+						) : (
+							<img src={plusIcon} alt="Plus Icon" />
+						)}
+					</div>
 				</div>
 			</div>
-			{isOpen && (
-				<div className="px-4 py-4 bg-darkGrayBg rounded-lg transition-all duration-500 mt-2">
-					<div className="mb-2 flex flex-row text-lightGray font-mono text-sm items-center">
-						<span>
-							<img
-								src={locationIcon}
-								alt="Location Icon"
-								className="w-auto h-4 mr-2"
-							/>
-						</span>
-						<span>{experience.location}</span>
-					</div>
-					<div className="flex flex-row items-center">
-						<div className="w-3/4">
-							<ul className="list-disc pl-4 mb-4">
-								{experience.responsibility.map(
-									(resp, index) => (
-										<li key={index} className="">
-											{resp}
-										</li>
-									)
-								)}
+			<div
+				className={`${
+					isOpen ? "max-h-full opacity-100" : "max-h-0 opacity-0"
+				} transition-all duration-500 ease-in-out overflow-hidden`}
+			>
+				{isOpen && (
+					<div className="px-4 py-4 bg-darkGrayBg rounded-lg transition-all duration-500 mt-2">
+						<div className="mb-2 flex flex-row text-lightGray font-mono text-sm items-center">
+							<span>
+								<img
+									src={locationIcon}
+									alt="Location Icon"
+									className="w-auto h-4 mr-2"
+								/>
+							</span>
+							<span>{experience.location}</span>
+						</div>
+						<div className="flex flex-row items-center">
+							<div className="w-3/4">
+								<ul className="list-disc pl-4 mb-4">
+									{experience.responsibility.map(
+										(resp, index) => (
+											<li key={index} className="">
+												{resp}
+											</li>
+										)
+									)}
+								</ul>
+							</div>
+							<div className="w-1/4 justify-center flex">
+								<img
+									src={experience.src}
+									alt={experience.alt}
+									className="h-auto w-44"
+								/>
+							</div>
+						</div>
+
+						<div className="font-medium my-1 w-3/4">
+							<ul className="flex flex-wrap flex-row">
+								{experience.techStack.map((exp, index) => (
+									<li
+										key={index}
+										className={`text-sm text-secondary bg-primary mr-2 my-1 px-4 py-1 rounded-2xl transition-all duration-300`}
+									>
+										{exp}
+									</li>
+								))}
 							</ul>
 						</div>
-						<div className="w-1/4 justify-center flex"><img src={experience.src} alt={experience.alt} className="h-auto w-44" /></div>
 					</div>
-
-					<div className="font-medium my-1 w-3/4">
-						<ul className="flex flex-wrap flex-row">
-							{experience.techStack.map((exp, index) => (
-								<li
-									key={index}
-									className={`text-sm text-secondary bg-primary mr-2 my-1 px-4 py-1 rounded-2xl transition-all duration-300`}
-								>
-									{exp}
-								</li>
-							))}
-						</ul>
-					</div>
-				</div>
-			)}
+				)}
+			</div>
 		</div>
 	);
 }
