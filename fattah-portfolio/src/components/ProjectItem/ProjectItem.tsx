d223a97ef5c18bb3db1943c14b05ce6d004847a4
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-19 00:33:22 +0800

    feat: add url link onclick projectItem

--- fattah-portfolio/src/components/ProjectItem/ProjectItem.tsx
+++ fattah-portfolio/src/components/ProjectItem/ProjectItem.tsx
@@ -5,53 +5,62 @@ import popupIcon from "../../assets/images/general/popup.svg";
 function ProjectItem({ project }: { project: Projects }) {
 	const [isHovered, setIsHovered] = useState<boolean>(false);
 
+	// LAZY LOAD THIS
+
 	return (
-		<div
-			className={`flex flex-col border-2 rounded-lg w-full min-h-auto bg-darkGrayBg text-start overflow-hidden transition-all duration-500 ${
-				isHovered ? "shadow-[0_0px_30px_5px_rgba(80,200,120,1.0)]" : ""
-			}`}
-			onMouseEnter={() => setIsHovered(true)}
-			onMouseLeave={() => setIsHovered(false)}
-		>
-			<div className="min-w-full w-full h-64 bg-lightBg flex justify-center overflow-hidden">
-				<img
-					src={project.src}
-					alt={project.alt}
-					className={`transform transition-transform duration-500 w-full h-full object-scale-down ${
-						isHovered ? "scale-110" : "scale-100"
-					}`}
-				/>
-			</div>
+		<a href={project.url} target="_blank" rel="noreferrer">
 			<div
-				className={`h-auto auto flex-grow px-5 py-5 flex flex-col justify-between ${
-					isHovered ? "text-greenLine" : "text-lightGray"
+				className={`flex flex-col border-2 rounded-lg w-full min-h-auto bg-darkGrayBg text-start overflow-hidden transition-all duration-500 ${
+					isHovered
+						? "shadow-[0_0px_30px_5px_rgba(80,200,120,1.0)]"
+						: ""
 				}`}
+				onMouseEnter={() => setIsHovered(true)}
+				onMouseLeave={() => setIsHovered(false)}
 			>
-				<div
-					className={
-						"font-semibold text-lg text-wrap transition-all duration-300 ease-in-out"
-					}
-				>
-					{project.title}
-				</div>
-				<div className="text-sm">
-					<span
-						className={`flex flex-row flex-wrap items-center transition-all duration-300 ease-in-out ${
-							isHovered ? "translate-x-2 text-greenLine" : ""
+				<div className="min-w-full w-full h-64 bg-lightBg flex justify-center overflow-hidden">
+					<img
+						src={project.src}
+						alt={project.alt}
+						className={`transform transition-transform duration-500 w-full h-full object-scale-down ${
+							isHovered ? "scale-110" : "scale-100"
 						}`}
-					>
-						Show project
-						{isHovered && (
-							<img
-								src={popupIcon}
-								alt="Popup Icon"
-								className={`w-auto h-3 ml-2 transition-all duration-1000 ease-in-out transform
-							${isHovered ? "scale-150 translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}
-							/>
-						)}
-					</span>
+					/>
 				</div>
-				{/* <div className="font-medium mt-4">
+				<div
+					className={`h-auto auto flex-grow px-5 py-5 flex flex-col justify-between ${
+						isHovered ? "text-greenLine" : "text-lightGray"
+					}`}
+				>
+					<div
+						className={
+							"font-semibold text-lg text-wrap transition-all duration-300 ease-in-out"
+						}
+					>
+						{project.title}
+					</div>
+					<div className="text-sm">
+						<span
+							className={`flex flex-row flex-wrap items-center transition-all duration-300 ease-in-out ${
+								isHovered ? "translate-x-2 text-greenLine" : ""
+							}`}
+						>
+							Show project
+							{isHovered && (
+								<img
+									src={popupIcon}
+									alt="Popup Icon"
+									className={`w-auto h-3 ml-2 transition-all duration-1000 ease-in-out transform
+							${
+								isHovered
+									? "scale-150 translate-x-0 opacity-100"
+									: "translate-x-full opacity-0"
+							}`}
+								/>
+							)}
+						</span>
+					</div>
+					{/* <div className="font-medium mt-4">
 					<ul className="flex flex-row justify-start overflow-x-auto whitespace-nowrap scroll-auto">
 						{project.techStack.map((tech, index) => (
 							<li
@@ -65,8 +74,9 @@ function ProjectItem({ project }: { project: Projects }) {
 						))}
 					</ul>
 				</div> */}
+				</div>
 			</div>
-		</div>
+		</a>
 	);
 }
 
