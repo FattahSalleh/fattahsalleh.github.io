38a0361a3d9e39abeae661ac3d48c9502c3f1224
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-18 22:44:02 +0800

    style: remove desc for ProjectItem and adjust stlying

--- fattah-portfolio/src/components/ProjectItem/ProjectItem.tsx
+++ fattah-portfolio/src/components/ProjectItem/ProjectItem.tsx
@@ -10,26 +10,28 @@ function ProjectItem({ project }: { project: Projects }) {
 			onMouseEnter={() => setIsHovered(true)}
 			onMouseLeave={() => setIsHovered(false)}
 		>
-			<div className="min-w-full w-full max-h-4/6 bg-lightBg flex justify-center overflow-hidden">
+			<div className="min-w-full w-full h-fit bg-lightBg flex justify-center overflow-hidden">
 				<img
 					src={project.src}
 					alt={project.alt}
-					className={`transform transition-transform duration-500 object-center h-96 object-scale-down ${
+					className={`transform transition-transform duration-500 w-full h-full object-cover ${
 						isHovered ? "scale-110" : "scale-100"
 					}`}
 				/>
 			</div>
-			<div className="flex-grow px-12 py-4 flex flex-col justify-between">
-				<div>
-					{/* className={`font-bold text-lg 
+			<div className="h-auto flex-grow px-12 py-4 flex flex-col justify-between">
+				<div
+					className={`font-bold text-lg 
                     ${
 						isHovered ? "text-wrap" : "truncate"
-                        } transition-all duration-1000 ease-in-out
-                    `} */}
-					<p className="font-bold text-lg">{project.title}</p>
-					<small className="mt-4 pt-4 font-mono">
+					} transition-all duration-1000 ease-in-out
+                    `}
+				>
+					{project.title}
+					{/* <p className="font-bold text-lg">{project.title}</p> */}
+					{/* <small className="mt-4 pt-4 font-mono">
 						{project.desc}
-					</small>
+					</small> */}
 				</div>
 				<div className="font-medium mt-auto">
 					<ul className="flex flex-wrap flex-row justify-start">
