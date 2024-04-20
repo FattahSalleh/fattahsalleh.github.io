f6b167eb0ba7833f94ee80c7e6c2b9ca676ce77c
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-21 01:38:51 +0800

    style: chang style of contactoverlay left

--- fattah-portfolio/src/components/Contact/ContactOverlay.tsx
+++ fattah-portfolio/src/components/Contact/ContactOverlay.tsx
@@ -4,12 +4,12 @@ import ContactOption from "./ContactOption";
 export default function ContactOverlay() {
 	return (
 		<div>
-			<div className="h-auto w-fit fixed bottom-0 left-8 right-auto z-10 flex flex-column flex-no-wrap justify-between items-center">
+			<div className="h-auto w-fit fixed bottom-0 left-12 right-auto z-10 flex flex-column flex-no-wrap justify-between items-center">
 				<ul>
 					{contactData.map((contact, index) => (
 						<ContactOption contact={contact} key={index} />
 					))}
-					<div className="block w-1 h-16 my-0 mx-auto bg-greenTurquoise"></div>
+					<div className="block w-0.5 h-24 my-0 mx-auto bg-greenTurquoise"></div>
 				</ul>
 			</div>
 		</div>
