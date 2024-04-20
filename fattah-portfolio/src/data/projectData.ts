d985c0e0e901f5991c8c767978007d5351bc1ace
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-21 05:50:34 +0800

    feat: add new assets and info for projectData

--- fattah-portfolio/src/data/projectData.ts
+++ fattah-portfolio/src/data/projectData.ts
@@ -2,6 +2,10 @@ import { Projects } from "../common/types";
 import allianceBizsmart from "../assets/images/project/alliance_bizsmart.png";
 import ocbcDuitNowQR from "../assets/images/project/ocbcduitnowqr-all-lg.png";
 import tmGlobal from "../assets/images/project/tmglobal-logo.png";
+import ocbccalc from "../assets/images/project/ocbccalc.jpg";
+import maybankpage from "../assets/images/project/maybankpage.jpg";
+import ocbcpage from "../assets/images/project/ocbcpage.jpg";
+import katsana from "../assets/images/project/katsana.jpg";
 
 export const projects: Projects[] = [
 	{
@@ -27,5 +31,37 @@ export const projects: Projects[] = [
 		desc: "It's simpler and faster with OCBC DuitNow QR. Simply scan or flash your DuitNow QR code for funds transfer or payments.",
 		techStack: ["Kotlin"],
 		url: "https://play.google.com/store/apps/details?id=com.ocbc.mobilebv.my&hl=en_ZA",
+	},
+	{
+		src: ocbccalc,
+		alt: "OCBC Malaysia Mortgage Calculator",
+		title: "OCBC Malaysia Mortgage Calculator",
+		desc: "",
+		techStack: [],
+		url: "https://www.ocbc.com.my/personal-banking/life-goals/mortgage-planner",
+	},
+	{
+		src: maybankpage,
+		alt: "Maybank Investment Banking",
+		title: "Maybank Investment Banking",
+		desc: "",
+		techStack: [],
+		url: "https://www.maybank.com/investment-banking/sg/index.html",
+	},
+	{
+		src: ocbcpage,
+		alt: "OCBC Group Malaysia Revamp",
+		title: "OCBC Group Malaysia Revamp",
+		desc: "",
+		techStack: [],
+		url: "https://www.ocbc.com.my/group/gateway",
+	},
+	{
+		src: katsana,
+		alt: "Katsana Platform Software",
+		title: "Katsana Platform Software",
+		desc: "",
+		techStack: [],
+		url: "https://my.katsana.com/login",
 	},
 ];
