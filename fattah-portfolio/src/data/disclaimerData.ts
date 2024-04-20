a23bcf9b2cbe2497757f4e0d227831811435c2f6
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-21 03:36:09 +0800

    chore: update disclaimerData with new assets

--- fattah-portfolio/src/data/disclaimerData.ts
+++ fattah-portfolio/src/data/disclaimerData.ts
@@ -1,34 +1,39 @@
 import { Disclaimer } from "../common/types";
+import figma from "../assets/images/disclaimer/figma.svg";
+import vscode from "../assets/images/disclaimer/vscode.svg";
+import react from "../assets/images/disclaimer/react.svg";
+import tailwind from "../assets/images/disclaimer/tailwind.svg";
+import vercel from "../assets/images/disclaimer/vercel.svg";
 
-export const disclaimerDetails: Disclaimer[] = [
+export const disclaimerData: Disclaimer[] = [
 	{
 		name: "Figma",
 		url: "https://www.figma.com/",
-		src: "",
+		src: figma,
 		alt: "Figma Icon",
 	},
 	{
 		name: "Visual Studio Code",
 		url: "https://code.visualstudio.com/",
-		src: "",
+		src: vscode,
 		alt: "VS Code Icon",
 	},
 	{
 		name: "React.js",
 		url: "https://react.dev/",
-		src: "",
+		src: react,
 		alt: "React.js Icon",
 	},
 	{
 		name: "Tailwind CSS",
 		url: "https://tailwindcss.com/",
-		src: "",
+		src: tailwind,
 		alt: "Tailwind CSS Icon",
 	},
 	{
 		name: "Vercel",
 		url: "https://vercel.com/",
-		src: "",
+		src: vercel,
 		alt: "Vercel Icon",
 	},
 ];
