c01d70a789d4f9c0cfc814c09f9490047e88e2ba
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-20 23:50:28 +0800

    chore: change current position title

--- fattah-portfolio/src/data/introDetails.ts
+++ fattah-portfolio/src/data/introDetails.ts
@@ -2,6 +2,6 @@ import { MyDetails } from "../common/types";
 
 export const introDetails: MyDetails = {
 	name: "Fattah Salleh",
-	position: "FULL STACK DEVELOPER \xa0&\xa0 SOFTWARE ENGINEER",
+	position: "WEB DEVELOPER . UI/UX DESIGNER",
 };
 
