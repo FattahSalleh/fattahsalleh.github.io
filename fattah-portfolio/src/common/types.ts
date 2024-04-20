7deb588ce0253e393001632bc5ea46dcd4a5c5c1
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-21 03:04:37 +0800

    feat: add Disclaimer interface

--- fattah-portfolio/src/common/types.ts
+++ fattah-portfolio/src/common/types.ts
@@ -1,51 +1,58 @@
 export interface NavBarButtons {
-    label: string;
-    sectionId: string;
+	label: string;
+	sectionId: string;
 }
 
 export interface MyDetails {
-    name: string;
-    position: string;
+	name: string;
+	position: string;
 }
 
 interface SubItems {
-    title: string;
-    list: string[];
+	title: string;
+	list: string[];
 }
 
 export interface Expertise {
 	src: string;
 	alt: string;
-    title: string;
-    titleColor: string;
+	title: string;
+	titleColor: string;
 	desc: string;
-    subItems: SubItems[];
+	subItems: SubItems[];
 }
 
 export interface Projects {
-    src: string;
-    alt: string;
-    title: string;
-    desc: string;
-    techStack: string[];
-    url: string;
+	src: string;
+	alt: string;
+	title: string;
+	desc: string;
+	techStack: string[];
+	url: string;
 }
 
 export interface Experience {
-    position: string;
-    company: string;
-    location: string;
-    timeRange: string;
-    responsibility: string[];
-    techStack: string[];
-    url: string;
-    src?: string;
-    alt?: string;
+	position: string;
+	company: string;
+	location: string;
+	timeRange: string;
+	responsibility: string[];
+	techStack: string[];
+	url: string;
+	src?: string;
+	alt?: string;
 }
 
 export interface Contact {
-    name: string;
-    url: string;
-    src: string;
-    alt: string;
+	name: string;
+	url: string;
+	src: string;
+	alt: string;
+}
+
+export interface Disclaimer {
+	name: string;
+	url: string;
+	src: string;
+	alt: string;
 }
