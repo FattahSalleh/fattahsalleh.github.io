e601862c98d7717040680b36a8b4f9f7816b60da
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-21 03:11:40 +0800

    feat: create disclaimerDetails data

---
+++ fattah-portfolio/src/data/disclaimerDetails.ts
@@ -0,0 +1,34 @@
+import { Disclaimer } from "../common/types";
+
+export const disclaimerDetails: Disclaimer[] = [
+	{
+		name: "Figma",
+		url: "https://www.figma.com/",
+		src: "",
+		alt: "Figma Icon",
+	},
+	{
+		name: "Visual Studio Code",
+		url: "https://code.visualstudio.com/",
+		src: "",
+		alt: "VS Code Icon",
+	},
+	{
+		name: "React.js",
+		url: "https://react.dev/",
+		src: "",
+		alt: "React.js Icon",
+	},
+	{
+		name: "Tailwind CSS",
+		url: "https://tailwindcss.com/",
+		src: "",
+		alt: "Tailwind CSS Icon",
+	},
+	{
+		name: "Vercel",
+		url: "https://vercel.com/",
+		src: "",
+		alt: "Vercel Icon",
+	},
+];
