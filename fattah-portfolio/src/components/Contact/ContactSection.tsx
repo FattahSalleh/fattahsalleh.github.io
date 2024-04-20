0276c1579a0f953ee88ade97368cdf8432b0801f
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-21 04:59:59 +0800

    chore: add assets and style for ContactSection

--- fattah-portfolio/src/components/Contact/ContactSection.tsx
+++ fattah-portfolio/src/components/Contact/ContactSection.tsx
@@ -1,4 +1,10 @@
-export default function contactSection() {
+import hello from "../../assets/images/disclaimer/hello.svg";
+import wave from "../../assets/images/disclaimer/wave.svg";
+import { useState } from "react";
+
+export default function ContactSection() {
+	const [isHovered, setIsHovered] = useState(false);
+
 	return (
 		<>
 			<p className="w-4/6 text-center">
@@ -10,42 +16,18 @@ export default function contactSection() {
 			<a
 				href="#_"
 				className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-darkText transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-lightGray group mt-16"
+				onMouseEnter={() => setIsHovered(true)}
+				onMouseLeave={() => setIsHovered(false)}
 			>
-				<span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-primary group-hover:h-full"></span>
+				<span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-secondary group-hover:h-full"></span>
 				<span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-					<svg
-						className="w-5 h-5 text-secondary"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M14 5l7 7m0 0l-7 7m7-7H3"
-						></path>
-					</svg>
+					<img src={wave} alt="wave" />
 				</span>
 				<span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-					<svg
-						className="w-5 h-5 text-secondary"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M14 5l7 7m0 0l-7 7m7-7H3"
-						></path>
-					</svg>
+					<img src={hello} alt="hello" />
 				</span>
-				<span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-lightText">
-					Come Say Hello
+				<span className="relative w-full text-left transition-colors duration-200 ease-in-out">
+					{isHovered ? "One more click!" : "Come say hello"}
 				</span>
 			</a>{" "}
 		</>
