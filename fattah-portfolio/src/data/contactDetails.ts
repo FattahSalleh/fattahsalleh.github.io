6d3396f7905db67e2f09189ec79140f31f1fac60
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-21 00:46:16 +0800

    feat: create and fill contactDetails

---
+++ fattah-portfolio/src/data/contactDetails.ts
@@ -0,0 +1,25 @@
+import { Contact } from "../common/types";
+import github from "../assets/images/contact/github.svg";
+import linkedin from "../assets/images/contact/linkedin.svg";
+import email from "../assets/images/contact/email.svg";
+
+export const contactData: Contact[] = [
+	{
+		name: "Github",
+		url: "https://github.com/FattahSalleh",
+		src: github,
+		alt: "Github Icon",
+	},
+	{
+		name: "LinkedIn",
+		url: "https://www.linkedin.com/in/fattah-salleh/",
+		src: linkedin,
+		alt: "LinkedIn Icon",
+	},
+	{
+		name: "Email",
+		url: "fattah.sallehuddin@gmail.com",
+		src: email,
+		alt: "Email Icon",
+	},
+];
