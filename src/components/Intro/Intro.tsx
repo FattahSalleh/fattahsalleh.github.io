50b9db4e2951a47f96adc2778c1c4c24d9a41e53
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-22 16:00:08 +0800

    style: add transition to Intro text

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,13 +1,21 @@
+import "./Intro.css";
+
 function Intro({ name, subTitle }: { name: string; subTitle: string }) {
 	return (
-		<>
-			<div className="font-bold h-full text-9xl tracking-wider leading-none drop-shadow-md uppercase">
+		<div className="text-center">
+			<div
+				id="introName"
+				className="font-bold h-full text-9xl tracking-wider leading-none drop-shadow-md uppercase"
+			>
 				{name}
 			</div>
-			<div className="font-semibold pt-8 text-lg tracking-widest">
+			<div
+				id="introSubTitle"
+				className="font-semibold pt-8 text-lg tracking-widest"
+			>
 				{subTitle}
 			</div>
-		</>
+		</div>
 	);
 }
 
