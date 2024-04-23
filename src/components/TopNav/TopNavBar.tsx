9694551867b80bc4a26f084375f03185a80966e7
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-23 16:00:12 +0800

    feat: add overlay function to TopNavBar mobile

--- src/components/TopNav/TopNavBar.tsx
+++ src/components/TopNav/TopNavBar.tsx
@@ -3,21 +3,29 @@ import fsIcon from "../../assets/images/fsicon/fs-icon.svg";
 import { topNavBarButtons } from "../../data/topNavBarData";
 import resume from "../../assets/pdf/Resume_FattahSalleh_2024.pdf";
 
-
-function TopNavBar() {
+export default function TopNavBar() {
 	const [isScrolled, setIsScrolled] = useState<boolean>(false);
+	const [isOverlayOpen, setIsOverlayOpen] = useState<boolean>(false);
 
 	const scrollToSection = (id: string) => {
 		const section = document.getElementById(id);
 		if (section) {
 			section.scrollIntoView({ behavior: "smooth" });
+			isOverlayOpen && closeOverlay();
 		}
+	};
+
+	const toggleOverlay = () => {
+		setIsOverlayOpen(!isOverlayOpen);
+	};
+
+	const closeOverlay = () => {
+		setIsOverlayOpen(false);
 	};
 
 	useEffect(() => {
 		const handleScroll = () => {
 			const scrollPosition = window.scrollY;
-			// Change background and text color when scrolled down
 			setIsScrolled(scrollPosition > 0);
 		};
 
@@ -28,45 +36,103 @@ function TopNavBar() {
 	}, []);
 
 	return (
-		<nav
-			className={`md:max-h-24 w-full md:flex flex-no-wrap fixed top-0 z-10 flex-row justify-between px-12 py-4 items-center transition-all duration-1000 
+		<>
+			{/* Desktop */}
+			<nav
+				className={`md:max-h-24 w-full md:flex flex-no-wrap fixed top-0 z-10 flex-row justify-between px-12 py-4 items-center transition-all duration-1000 
 			hidden 
             ${
 				isScrolled
 					? "bg-darkBg text-secondary opacity-95"
 					: "bg-darkBg text-lightText"
 			}`}
-			id="topNavBar"
-		>
-			<div>
-				<img
-					src={fsIcon}
-					alt="FS Icon"
-					width={32}
-					height={32}
-					className={`transition-all duration-1000 ${
-						isScrolled ? "opacity-0" : "opacity-100"
-					}`}
-				/>
-			</div>
-			<div className="flex flex-row justify-between font-semibold items-center">
-				{topNavBarButtons.map((button, index) => (
-					<button
-						className="mx-4"
-						key={index}
-						onClick={() => scrollToSection(button.sectionId)}
-					>
-						{button.label}
+				id="topNavBar"
+			>
+				<div>
+					<img
+						src={fsIcon}
+						alt="FS Icon"
+						width={32}
+						height={32}
+						className={`transition-all duration-1000 ${
+							isScrolled ? "opacity-0" : "opacity-100"
+						}`}
+					/>
+				</div>
+				<div className="flex flex-row justify-between font-semibold items-center">
+					{topNavBarButtons.map((button, index) => (
+						<button
+							className="mx-4"
+							key={index}
+							onClick={() => scrollToSection(button.sectionId)}
+						>
+							{button.label}
+						</button>
+					))}
+				</div>
+				<div className="border rounded py-2 px-4 hover:shadow-[0_0px_10px_5px_rgba(80,200,120,1.0)] hover:border-greenTurquoise transition-all">
+					<a href={resume} target="_blank" rel="noreferrer">
+						Resume
+					</a>
+				</div>
+			</nav>
+
+			{/* Mobile */}
+			<nav
+				className={`md:max-h-24 w-full md:hidden flex-no-wrap fixed top-0 z-10 flex-row justify-between px-12 py-4 items-center`}
+				id="topNavBar"
+			>
+				<div
+					className={`${
+						isScrolled ? "-translate-y-full" : ""
+					} transition-all duration-1000`}
+				>
+					<button onClick={toggleOverlay}>
+						<img
+							src={fsIcon}
+							alt="FS Icon"
+							width={32}
+							height={32}
+							className={`transition-all duration-1000 ${
+								isScrolled ? "opacity-0" : "opacity-100"
+							}`}
+						/>
 					</button>
-				))}
-			</div>
-			<div className="border rounded py-2 px-4 hover:shadow-[0_0px_10px_5px_rgba(80,200,120,1.0)] hover:border-greenTurquoise transition-all">
-				<a href={resume} target="_blank" rel="noreferrer">
-					Resume
-				</a>
-			</div>
-		</nav>
+				</div>
+
+				{isOverlayOpen && (
+					<div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-filter backdrop-blur-lg flex flex-col justify-center items-center z-20">
+						<button
+							className="absolute top-6 right-6 text-white text-2xl"
+							onClick={toggleOverlay}
+						>
+							X
+						</button>
+						<div className="flex flex-col justify-center items-center text-white">
+							{topNavBarButtons.map((button, index) => (
+								<button
+									className="my-4"
+									key={index}
+									onClick={() => {
+										scrollToSection(button.sectionId);
+									}}
+								>
+									{button.label}
+								</button>
+							))}
+							<div className="border rounded py-2 px-4 hover:shadow-[0_0px_10px_5px_rgba(80,200,120,1.0)] hover:border-greenTurquoise transition-all">
+								<a
+									href={resume}
+									target="_blank"
+									rel="noreferrer"
+								>
+									Resume
+								</a>
+							</div>
+						</div>
+					</div>
+				)}
+			</nav>
+		</>
 	);
 }
-
-export default TopNavBar;
