df45251b3f21e7ee2b05f4e75f8343dcf7b10d7f
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-23 14:42:54 +0800

    style: add responsive mobile to Intro and NavBar

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -4,19 +4,19 @@ import { introDetails } from "../../data/introData";
 function Intro() {
 	return (
 		<section
-			className="w-full min-h-screen flex flex-col items-center justify-center pb-8 md:pb-24"
+			className="w-full h-[calc(100dvh)] md:min-h-screen flex flex-col items-center justify-center pb-8 md:pb-24 text-center"
 			id="introSection"
 		>
 			<div className="text-center">
 				<div
 					id="introName"
-					className="font-bold text-4xl md:text-9xl tracking-wider leading-none drop-shadow-md uppercase"
+					className="font-bold text-lg md:text-9xl tracking-wider leading-none drop-shadow-md uppercase overflow-hidden whitespace-nowrap"
 				>
 					{introDetails.name}
 				</div>
 				<div
 					id="introSubTitle"
-					className="font-semibold pt-4 md:pt-8 text-base md:text-lg tracking-widest"
+					className="font-semibold pt-4 md:pt-8 text-base md:text-lg tracking-widest overflow-hidden whitespace-nowrap"
 				>
 					{introDetails.position}
 				</div>
--- src/components/TopNav/TopNavBar.tsx
+++ src/components/TopNav/TopNavBar.tsx
@@ -28,8 +28,8 @@ function TopNavBar() {
 
 	return (
 		<nav
-			className={`max-h-24 w-full md:flex flex-no-wrap fixed top-0 z-10 flex-row justify-between px-12 py-4 items-center transition-all duration-1000 
-			hidden
+			className={`md:max-h-24 w-full md:flex flex-no-wrap fixed top-0 z-10 flex-row justify-between px-12 py-4 items-center transition-all duration-1000 
+			hidden 
             ${
 				isScrolled
 					? "bg-darkBg text-secondary opacity-95"
