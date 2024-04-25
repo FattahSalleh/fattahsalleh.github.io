c34b8b550b453e41eb289f8b8ea45d6f76f1276f
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-25 11:57:27 +0800

    style: adjust Projects Done for mobile view

--- src/components/Project/ProjectSection.tsx
+++ src/components/Project/ProjectSection.tsx
@@ -5,7 +5,7 @@ export default function ProjectSection() {
 	return (
 		<section className="max-w-full w-full px-32 py-16" id="projectSection">
 			<div className="flex flex-col items-center">
-				<p className="font-bold h-full text-6xl drop-shadow-md mb-12">
+				<p className="font-bold h-full text-4xl md:text-6xl drop-shadow-md mb-12">
 					Projects Done
 				</p>
 				<article className="grid grid-cols-1 justify-items-start gap-4 md:grid-cols-3">
