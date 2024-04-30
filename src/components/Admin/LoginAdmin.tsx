9b2a52c5f9651a3e623fc5c11507f9274cd65f46
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-04-30 15:26:33 +0800

    feat: add react router and admin component route

---
+++ src/components/Admin/LoginAdmin.tsx
@@ -0,0 +1,8 @@
+export default function LoginAdmin() {
+	return (
+		<div>
+			{/* Add login form or other admin-related content here */}
+			<h2>Admin Page</h2>
+		</div>
+	);
+}
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,5 @@
+import { ReactNode, createContext, useContext, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Intro from "../Intro/Intro";
 import NavBar from "../Navigation/NavBar";
 import ContactOverlay from "../Contact/ContactOverlay";
@@ -6,45 +8,73 @@ import ContactSection from "../Contact/ContactSection";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import ExpertiseSection from "../Expertise/ExpertiseSection";
 import ProjectSection from "../Project/ProjectSection";
-import ExperiseSection from "../Experience/ExperienceSection";
-import { useState } from "react";
+import ExperienceSection from "../Experience/ExperienceSection";
 import Cursor from "../Cursor/Cursor";
 import ScrollToTop from "../Navigation/ScrollToTop";
+import LoginAdmin from "../Admin/LoginAdmin";
 
-function App() {
+interface OverlayContextProps {
+	isOverlayOpen: boolean;
+	setIsOverlayOpen: (isOpen: boolean) => void;
+}
 
+export const OverlayContext = createContext<OverlayContextProps>({
+	isOverlayOpen: false,
+	setIsOverlayOpen: () => {},
+});
+
+export const OverlayProvider: React.FC<{ children: ReactNode }> = ({
+	children,
+}) => {
 	const [isOverlayOpen, setIsOverlayOpen] = useState<boolean>(false);
 
 	return (
-		<div
-			className={`App flex min-h-screen flex-col items-center justify-between bg-darkBg text-lightText overflow-x-clip`}
-		>
-			<Cursor />
+		<OverlayContext.Provider value={{ isOverlayOpen, setIsOverlayOpen }}>
+			{children}
+		</OverlayContext.Provider>
+	);
+};
 
-			<ScrollToTop />
+function App() {
+	const [isOverlayOpen, setIsOverlayOpen] = useState<boolean>(false);
 
-			<ContactOverlay />
+	return (
+		<OverlayProvider>
+			<BrowserRouter>
+				<div
+					className={`App flex min-h-screen flex-col items-center justify-between bg-darkBg text-lightText overflow-x-clip`}
+				>
+					<Cursor />
+					<ScrollToTop />
+					<ContactOverlay />
+					<NavBar
+						isOverlayOpen={isOverlayOpen}
+						setIsOverlayOpen={setIsOverlayOpen}
+					/>
+					<div className={`${isOverlayOpen ? "blur" : ""}`}>
+						<Routes>
+							<Route path="/admin" element={<LoginAdmin />} />
+							<Route path="/" element={<PortfolioContent />} />
+						</Routes>
+					</div>
+					<SpeedInsights />
+				</div>
+			</BrowserRouter>
+		</OverlayProvider>
+	);
+}
 
-			<NavBar
-				isOverlayOpen={isOverlayOpen}
-				setIsOverlayOpen={setIsOverlayOpen}
-			/>
+function PortfolioContent() {
+	const { isOverlayOpen } = useContext(OverlayContext);
 
-			<div className={`${isOverlayOpen ? "blur" : ""}`}>
-				<Intro />
-
-				<ExpertiseSection />
-
-				<ProjectSection />
-
-				<ExperiseSection />
-
-				<ContactSection />
-
-				<DisclaimerSection />
-			</div>
-
-			<SpeedInsights />
+	return (
+		<div className={`${isOverlayOpen ? "blur" : ""}`}>
+			<Intro />
+			<ExpertiseSection />
+			<ProjectSection />
+			<ExperienceSection />
+			<ContactSection />
+			<DisclaimerSection />
 		</div>
 	);
 }
