188e32bd7d5e483bde6414304531a51b6cf700d9
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-05-13 15:00:22 +0800

    refactor: change database folder name to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,3 +22,8 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+onAuthStateChanged(auth, user => {
+	console.log("You are logged in as: " + user);
+})
+
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import './database/firebase'
+import './firebase/firebase'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
