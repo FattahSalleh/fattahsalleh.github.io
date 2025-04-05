53a07e2a85d59bd2591ab7f11e9bc939e4290e5e
Fattah Salleh <fattah.sallehuddin@gmail.com>
2025-04-06 00:33:28 +0800

    feat: add ButtonTimer component

--- src/components/Practice/ButtonTimer/ButtonTimer.tsx
+++ src/components/Practice/ButtonTimer/ButtonTimer.tsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from 'react';
+
 export function ButtonTimer() {
-  return <div>Button Timer</div>;
+  const [count, setCount] = useState<number>(0);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+  const TIMER_LIMIT = 2;
+
+  const onClick = () => {
+    setIsDisabled(true);
+  };
+
+  useEffect(() => {
+    if (isDisabled) {
+      const interval = setInterval(() => {
+        setCount(prevCount => {
+          const newCount = prevCount + 1;
+
+          if (newCount >= TIMER_LIMIT) {
+            setIsDisabled(false);
+            return 0;
+          }
+
+          return newCount;
+        });
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isDisabled]);
+
+  return (
+    <button
+      onClick={onClick}
+      className="border-2 p-2 rounded disabled:border-errorRed "
+      disabled={isDisabled}
+    >
+      {isDisabled ? <span>LOADING...</span> : <span>CLICK HERE</span>}
+      <br />
+      Counter: {count}
+    </button>
+  );
 }
